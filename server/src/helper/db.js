2bb04aebb646b458e4dbd98a6145ba5e2f713c60
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2026-08-31 16:59:48 +0530

    small changes

--- server/src/helper/db.js
+++ server/src/helper/db.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose"
+import dotenv from "dotenv"
 
-const connectDB =async()=>{
-    try{
-        const url="mongodb://127.0.0.1:27017/car_rental_app";
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const url = process.env.MONGO_URI;
         await mongoose.connect(url);
         console.log("Connected to DB");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         console.log("Error while Connecting to DB");
-    }  
+    }
 };
 export default connectDB;
